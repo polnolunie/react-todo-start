c21124881b27e4a378441e91b63a8ea5ffc9a368
polnolunie <polnolunie255@gmail.com>
2026-06-12 08:19:36 +0900

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import "./index.css";
 import NewTaskForm from "./components/NewTaskForm";
 import TaskList from "./components/TaskList";
 import Footer from "./components/Footer";
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -10,31 +10,41 @@ export default function Task({
   const [value, setValue] = useState(task.title);
 
   return (
-    <li>
+    <li
+      className={`${task.completed ? "completed" : ""} ${task.edit ? "editing" : ""}`.trim()}
+    >
       <input
+        className="toggle"
         type="checkbox"
         checked={task.completed}
         onChange={() => toggleTask(task.id)}
       />
       {task.edit ? (
-        <>
-          <input
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            onKeyDown={(e) => {
-              console.log(e);
-              if (e.key === "Enter") {
-                saveTask(task.id, value);
-              }
-            }}
-          />
-          <button onClick={() => editTask(task.id)}>Edit</button>
-        </>
+        <input
+          className="edit"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={(e) => {
+            console.log(e);
+            if (e.key === "Enter") {
+              saveTask(task.id, value);
+            }
+          }}
+        />
       ) : (
         <>
-          <span>{task.title}</span>
-          <button onClick={() => editTask(task.id)}>Edit</button>
-          <button onClick={() => deleteTask(task.id)}>Delete</button>
+          <label>
+            <span className="description">{task.title}</span>
+          </label>
+
+          <button
+            className="icon icon-edit"
+            onClick={() => editTask(task.id)}
+          />
+          <button
+            className="icon icon-destroy"
+            onClick={() => deleteTask(task.id)}
+          />
         </>
       )}
     </li>
